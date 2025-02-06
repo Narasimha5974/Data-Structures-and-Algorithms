a0a0a7cf29db75f6b536ab5958fd97fb9296dd3b
MANCHI LAKSHMI NARASIMHULU <m.narasimhans5974@gmail.com>
2025-02-06 20:20:54 +0530

    FINDING EVEN OR ODD USING ARRAYS.c

--- FINDING EVEN OR ODD USING ARRAYS.c
+++ FINDING EVEN OR ODD USING ARRAYS.c
@@ -1,10 +1,12 @@
- #include<stdio.h>
+#include<stdio.h>
   int main(){
-  int arr[5]={1,2,3,4,5};
-  for(int i=0;i<5;i++){
-   if (num/2)
-      printf("%d",even);
-    else
-      printf("%d",odd);
+  int arr[5]={7,2,3,9,5};
+  for(int i=0; i<5; i++){
+   if (arr[i]%2==0){
+      printf("EVEN\n");
+      }
+    else{
+      printf("ODD\n");
   }
-    }
+     }
+  }
