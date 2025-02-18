cc95e1b323e5f67febcc709c4cbb829b8cc35538
MANCHI LAKSHMI NARASIMHULU <m.narasimhans5974@gmail.com>
2025-02-18 14:28:29 +0530

    INSERTION SORT.c

--- INSERTION SORT.c
+++ INSERTION SORT.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main()
 {
-   int arr[5]={5,4,3,2,1};
+   int arr[5]={2,3,4,1,5};
    int k;
    for(int i=1;i<=4;i++){
      int temp=arr[i];
+     k=i;
      for(int j=i-1;j>=0;j--){
        if(arr[j]>temp){
          arr[j+1]=arr[j];
